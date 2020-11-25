a50a6ad0602f592cb90c48b9092a8568b6be476c
Zak <rusty_clarinet@yahoo.com>
2020-11-25 11:19:37 -0500

    First Commit

---
+++ Assets/_Scripts/EnemyBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    Animator anim;
+
+    enum EnemyState { stand, down}
+
+    private EnemyState enemyState = EnemyState.stand;
+
+    private float wakeDelay = 3f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "WeaponPoint" && enemyState == EnemyState.stand);
+        {
+            anim.SetTrigger("die");
+            enemyState = EnemyState.down;
+            Die();
+            Debug.Log("collide with " + col.gameObject.name);
+        }
+    }
+
+    private void Die()
+    {
+        if(enemyState == EnemyState.down)
+        {
+            StartCoroutine("WakeUp");
+        }
+    }
+
+    IEnumerator WakeUp()
+    {
+        yield return new WaitForSeconds (wakeDelay);
+        anim.SetTrigger("up");
+        enemyState = EnemyState.stand;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/HandPresence.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+
+            }
+            else
+            {
+                Debug.Log("Controller model not found");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+
+        if(showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
+        
+        /*
+        if(targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+        {
+            //Primary button action (A)
+        }
+
+        
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+        {
+            //Trigger action
+        }
+
+        
+        if(targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+        {
+            //Analog thumb action
+        }
+        */
+    }
+
+}
